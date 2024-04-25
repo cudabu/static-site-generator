fc638ec9a30188444a68a63be1d7ab69b8cac875
Curtis Bunch <me@cudabu.io>
2024-04-25 08:34:23 -0500

    HTMLnode and tests

--- src/main.py
+++ src/main.py
@@ -1,8 +1,12 @@
 from textnode import TextNode
+from htmlnode import HTMLNode
 
 
 def main():
     node = TextNode("This is a test node", "bold", "https://www.cudabu.io")
-    print(noded)
+    print(node)
+
+    node = HTMLNode(tag='a', value='www.cudabu.io', children=[], props={'href': 'https://www.cudabu.io', 'target': '_blank'})
+    print(node)
 
 main()
---
+++ src/test_htmlnode.py
@@ -0,0 +1,24 @@
+import unittest
+
+from htmlnode import (
+    HTMLNode
+)
+
+class TestHTMLNode(unittest.TestCase):
+    def test_init(self):
+        node = HTMLNode(tag='div', value='Hello', children=[], props={'class': 'container'})
+        self.assertEqual(node.tag, 'div')
+        self.assertEqual(node.value, 'Hello')
+        self.assertEqual(node.children, [])
+        self.assertEqual(node.props, {'class': 'container'})
+
+    def test_to_html(self):
+        node = HTMLNode(tag='p', value='Lorem ipsum', children=[], props=None)
+        self.assertEqual(node.to_html(), NotImplemented)
+
+    def test_props_to_html(self):
+        node = HTMLNode(tag='a', value='Click me', children=[], props={'href': 'https://example.com'})
+        self.assertEqual(node.props_to_html(), ' href="https://example.com"')
+
+if __name__ == '__main__':
+    unittest.main()
