dd335956da290a09a0503d6c3cbe4878bc666f54
Curtis Bunch <me@cudabu.io>
2024-04-27 11:02:32 -0500

    updated tests

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -3,21 +3,6 @@ from htmlnode import LeafNode, ParentNode, HTMLNode
 
 
 class TestHTMLNode(unittest.TestCase):
-    """
-    A test case for the HTMLNode class.
-
-    This test case includes various test methods to verify the functionality of the HTMLNode class.
-
-    Test methods:
-    - test_to_html_props: Tests the `props_to_html` method of HTMLNode.
-    - test_to_html_no_children: Tests the `to_html` method of LeafNode when there are no children.
-    - test_to_html_no_tag: Tests the `to_html` method of LeafNode when there is no tag.
-    - test_to_html_with_children: Tests the `to_html` method of ParentNode when there are children.
-    - test_to_html_with_grandchildren: Tests the `to_html` method of ParentNode when there are grandchildren.
-    - test_to_html_many_children: Tests the `to_html` method of ParentNode when there are many children.
-    - test_headings: Tests the `to_html` method of ParentNode for headings.
-    """
-
     def test_to_html_props(self):
         node = HTMLNode(
             "div",
--- src/test_textnode.py
+++ src/test_textnode.py
@@ -9,7 +9,6 @@ from textnode import (
     text_type_image,
     text_type_link,
 )
-from textnode import TextNode
 
 
 class TestTextNode(unittest.TestCase):
@@ -20,85 +19,25 @@ class TestTextNode(unittest.TestCase):
 
     def test_eq_false(self):
         node = TextNode("This is a text node", text_type_text)
-        node2 = TextNode("This is a bold node", text_type_bold)
+        node2 = TextNode("This is a text node", text_type_bold)
         self.assertNotEqual(node, node2)
-
 
     def test_eq_false2(self):
         node = TextNode("This is a text node", text_type_text)
-        node2 = TextNode("This is a bold node2", text_type_text)
+        node2 = TextNode("This is a text node2", text_type_text)
         self.assertNotEqual(node, node2)
 
     def test_eq_url(self):
-        node = TextNode("This is a text node", text_type_italic, "https://www.cudabu.io")
-        node2 = TextNode("This is a text node", text_type_italic, "https://www.cudabu.io")
+        node = TextNode("This is a text node", text_type_text, "https://www.boot.dev")
+        node2 = TextNode("This is a text node", text_type_text, "https://www.boot.dev")
         self.assertEqual(node, node2)
 
-    def test_eq_url_false(self):
-        node = TextNode("This is a text node", text_type_italic)
-        node2 = TextNode("This is a text node", text_type_italic, "https://www.cudabu.io")
-        self.assertNotEqual(node, node2)
+    def test_repr(self):
+        node = TextNode("This is a text node", text_type_text, "https://www.boot.dev")
+        self.assertEqual(
+            "TextNode(This is a text node, text, https://www.boot.dev)", repr(node)
+        )
 
-    def test_report(self):
-        node = TextNode("This is a text node", text_type_text, "https://www.cudabu.io")
-        self.assertEqual("TextNode(This is a text node, text, https://www.cudabu.io)", repr(node))
-
-if __name__ == "__main__":
-    unittest.main()
-import unittest
-from textnode import (
-    TextNode,
-    text_type_text,
-    text_type_bold,
-    text_type_italic,
-    text_type_code,
-    text_type_image,
-    text_type_link,
-)
-from textnode import TextNode
-from leafnode import LeafNode
-
-class TestTextNode(unittest.TestCase):
-    def test_text_node_to_html_node_text(self):
-        text_node = TextNode("This is a text node", text_type_text)
-        html_node = text_node_to_html_node(text_node)
-        expected_html_node = LeafNode(tag=None, value="This is a text node")
-        self.assertEqual(html_node, expected_html_node)
-
-    def test_text_node_to_html_node_bold(self):
-        text_node = TextNode("This is a bold node", text_type_bold)
-        html_node = text_node_to_html_node(text_node)
-        expected_html_node = LeafNode(tag='b', value="This is a bold node")
-        self.assertEqual(html_node, expected_html_node)
-
-    def test_text_node_to_html_node_italic(self):
-        text_node = TextNode("This is an italic node", text_type_italic)
-        html_node = text_node_to_html_node(text_node)
-        expected_html_node = LeafNode(tag='i', value="This is an italic node")
-        self.assertEqual(html_node, expected_html_node)
-
-    def test_text_node_to_html_node_code(self):
-        text_node = TextNode("This is a code node", text_type_code)
-        html_node = text_node_to_html_node(text_node)
-        expected_html_node = LeafNode(tag='code', value="This is a code node")
-        self.assertEqual(html_node, expected_html_node)
-
-    def test_text_node_to_html_node_link(self):
-        text_node = TextNode("This is a link node", text_type_link, "https://www.example.com")
-        html_node = text_node_to_html_node(text_node)
-        expected_html_node = LeafNode(tag='a', value="This is a link node", props={'href': 'https://www.example.com', 'target': '_blank'})
-        self.assertEqual(html_node, expected_html_node)
-
-    def test_text_node_to_html_node_image(self):
-        text_node = TextNode("This is an image node", text_type_image, "https://www.example.com/image.jpg")
-        html_node = text_node_to_html_node(text_node)
-        expected_html_node = LeafNode(tag='img', value=None, props={'src': 'https://www.example.com/image.jpg', 'alt': 'This is an image node'})
-        self.assertEqual(html_node, expected_html_node)
-
-    def test_text_node_to_html_node_invalid_type(self):
-        text_node = TextNode("This is an invalid node", "invalid_type")
-        with self.assertRaises(ValueError):
-            text_node_to_html_node(text_node)
 
 if __name__ == "__main__":
     unittest.main()
