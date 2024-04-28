776c70322befa2891dea616edccd6e7187ff5a77
Curtis Bunch <me@cudabu.io>
2024-04-28 16:38:37 -0500

    i forget exactly

--- src/textnode.py
+++ src/textnode.py
@@ -37,13 +37,3 @@ def text_node_to_html_node(text_node):
     else:
         raise ValueError(f"Invalid text type: {text_node.text_type}")
 
-def split_nodes_delimiter(old_nodes, delimiter, text_type):
-    new_nodes = []
-    for node in old_nodes:
-        if node.text_type == text_type_text:
-            new_nodes.extend(
-                TextNode(text, text_type_text)
-                for text in node.text.split(delimiter)
-            )
-        else:
-            new_nodes.append(node)
